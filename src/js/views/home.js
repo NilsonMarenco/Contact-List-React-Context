320227072cd006f361d54db67ff81139fd882c57
NilsonMarenco <137912131+NilsonMarenco@users.noreply.github.com>
2024-03-17 20:54:21 +0000

    Creando Rutas

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,14 +1,31 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
-import "../../styles/home.css";
+import React, { useContext, useEffect, useState } from "react";
+import { Context } from "../store/appContext";
+import { Link, Navigate } from "react-router-dom";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>My Contact List!</h1>
-		<p>
-		</p>
-		<a href="/add-contacts" className="btn btn-success">
-			Add Contact
-		</a>
-	</div>
-);
+export const Home = () => {
+    const { store } = useContext(Context);
+    const [redirectToContacts, setRedirectToContacts] = useState(false);
+
+    useEffect(() => {
+        // Verificar si hay contactos cargados en el estado
+        if (store.Contacts && store.Contacts.length > 0) {
+            setRedirectToContacts(true);
+        }
+    }, [store.Contacts]);
+
+    return (
+        <div className="text-center mt-5">
+            {redirectToContacts ? (
+                // Redirigir a la página de contactos si hay contactos cargados
+                <Navigate to="/contacts" />
+            ) : (
+                // Renderizar el contenido de la página de inicio
+                <>
+                    <h1>My Contact List!</h1>
+                    <p>Welcome to your contact list. Here you can manage your contacts.</p>
+                    <Link to="/add-contacts" className="btn btn-success">Add Contact</Link>
+                </>
+            )}
+        </div>
+    );
+};
